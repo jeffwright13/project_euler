24c373025712c605512eaf597385f5c6a254f026
Jeff Wright <jeff.washcloth@gmail.com>
2016-03-01 08:01:45 -0500

    Solution in place and tested

--- euler1.py
+++ euler1.py
@@ -3,7 +3,11 @@ def main():
     print function(1000)
 
 def function(max):
-    pass
+    count = 0
+    for i in range(max):
+        if i % 3 == 0 or i % 5 == 0:
+            count += i
+    return count
 
 def test_function():
     assert function(10)   == 23
