56746672f48cb98ff48ab8e383646a6426c94f65
Jeff Wright <jeff.washcloth@gmail.com>
2016-03-01 16:41:21 -0500

    New function is_product(). With test code that fails since it is not implemented.

--- euler4.py
+++ euler4.py
@@ -1,5 +1,17 @@
 def main():
-    pass
+    # Enumerate all palindromes less than 999*999
+    palindromes = []
+    for candidate in range(998001, 12321, -1):
+        if is_palindrome(candidate):
+            palindromes.append(candidate)
+    print palindromes
+
+    # Find which are the products of two 3-digit numbers
+    factors = []
+    for elem in palindromes:
+        if is_product(elem):
+            factors.append(elem)
+    print factors
 
 def is_palindrome(num):
     numstr = str(num)
@@ -13,6 +25,9 @@ def is_palindrome(num):
     else:
         return False
 
+def is_product(num):
+    pass
+
 def test_is_palindrome():
     assert is_palindrome(1)    == None
     assert is_palindrome(11)   == True
@@ -20,5 +35,8 @@ def test_is_palindrome():
     assert is_palindrome(2111) == False
     assert is_palindrome(2112) == True
 
+def test_is_product():
+    assert is_product(997799)  == True
+    
 if __name__ == "__main__":
     main()
