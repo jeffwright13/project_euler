e59ed57a5f6a9f02aac0fde417aa5d79b5e4a3cb
Jeff Wright <jeff.washcloth@gmail.com>
2016-03-01 16:27:42 -0500

    Implementation and test code for is_palindrome() function

--- euler4.py
+++ euler4.py
@@ -2,13 +2,23 @@ def main():
     pass
 
 def is_palindrome(num):
-    if len(num) % 2 != 0:
+    numstr = str(num)
+    if len(numstr) % 2 != 0:
+        return None
+
+    first = numstr[:len(numstr)/2]
+    last  = numstr[len(numstr)/2:]
+    if first == last[::-1]:
+        return True
+    else:
         return False
 
 def test_is_palindrome():
-    assert is_palindrome(1)  == True
-    assert is_palindrome(11) == True
-    assert is_palindrome(21) == False
+    assert is_palindrome(1)    == None
+    assert is_palindrome(11)   == True
+    assert is_palindrome(21)   == False
+    assert is_palindrome(2111) == False
+    assert is_palindrome(2112) == True
 
 if __name__ == "__main__":
     main()
