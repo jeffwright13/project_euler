4b6e758a89647e483ed6c17dd4b53df16f6239be
Jeff Wright <jeff.washcloth@gmail.com>
2016-03-02 13:00:13 -0500

    Working and tested for Euler5

--- euler5.py
+++ euler5.py
@@ -1,11 +1,30 @@
 def main():
-    pass
+    print smallest_evenly_divisible(1, 20)
 
-def is_palindrome(num):
-    pass
+def smallest_evenly_divisible(lower, upper):
+    if lower <= 0:
+        return None
+
+    candidate = upper + 1
+    done = False
+    while 1:
+        for elem in range(lower, upper + 1):
+            if candidate % elem != 0:
+                break
+            else:
+                if elem == upper:
+                    done = True
+                
+        if done:
+            break
+        candidate += 1
+    return candidate
     
-def test_is_palindrome():
-    assert is_palindrome(1) == None
+def test_smallest_evenly_divisible():
+    assert smallest_evenly_divisible(0, 1)    == None
+    assert smallest_evenly_divisible(1, 3)    == 6
+    assert smallest_evenly_divisible(2, 4)    == 12
+    assert smallest_evenly_divisible(1, 10)   == 2520
 
 if __name__ == "__main__":
     main()
