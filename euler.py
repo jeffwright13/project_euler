d2639aff01bee4d6d72ad5c3dc26587be68c849c
Jeff Wright <jeff.washcloth@gmail.com>
2016-03-04 09:28:47 -0500

    Generic code structure (with main(), function(), test code)

---
+++ euler.py
@@ -0,0 +1,15 @@
+def main():
+    print function(None)
+
+def function(var):
+    """
+    Generic function
+    """
+    return None
+
+def test_function():
+    assert function(None) == None
+
+if __name__ == "__main__":
+    main()
+    
